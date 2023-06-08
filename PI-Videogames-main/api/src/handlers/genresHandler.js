40f3d1e7b50e0a489845cc8f36dffd2b865a07b8
Jose Lizandro Vargas Ollisco <vjose2807@gmail.com>
2023-06-07 23:39:06 -0400

    detail solucionado

--- PI-Videogames-main/api/src/handlers/genresHandler.js
+++ PI-Videogames-main/api/src/handlers/genresHandler.js
@@ -2,10 +2,7 @@ const createGenre = require("../controllers/genresController");
 const vgGenresHandler = async (req, res) => {
   try {
     const newGenre = await createGenre();
-    res.status(201).json({
-      success: true,
-      message: "Los géneros se han cargado correctamente",
-    });
+    res.status(200).json(newGenre);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
