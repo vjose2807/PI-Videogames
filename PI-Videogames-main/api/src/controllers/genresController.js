02e8581e011b79287c965e7e86aac104e3fe1b84
Jose Lizandro Vargas Ollisco <vjose2807@gmail.com>
2023-06-13 11:28:38 -0400

    Reparado (no se podia hacer mas de una peticion)

--- PI-Videogames-main/api/src/controllers/genresController.js
+++ PI-Videogames-main/api/src/controllers/genresController.js
@@ -1,18 +1,18 @@
 const axios = require("axios");
-const { API_KEY } = process.env;
 const { Genre } = require("../db.js");
 const cleanGenre = require("../utils/cleanGenre.js");
+const { API_KEY } = process.env;
 
 const getGenres = async () => {
+  const genresResponse = (
+    await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
+  ).data.results;
+  const genresFromAPI = cleanGenre(genresResponse);
   const genresFromDB = await Genre.findAll();
   if (genresFromDB.length === 0) {
-    const genresResponse = (
-      await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
-    ).data.results;
-    const genresFromAPI = cleanGenre(genresResponse);
     await Genre.bulkCreate(genresFromAPI);
-    return genresFromAPI;
   }
+  return genresFromAPI;
 };
 
 module.exports = { getGenres };
