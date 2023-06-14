d0d4aefad2564bc46c9bb3ef88c41fa6dea2688a
Jose Lizandro Vargas Ollisco <vjose2807@gmail.com>
2023-06-14 01:13:01 -0400

    solucionado (no mostraba platforms ni genres para seleccionar)

--- PI-Videogames-main/client/src/views/Form/Form.jsx
+++ PI-Videogames-main/client/src/views/Form/Form.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import styles from "./Form.module.css";
+
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 import { createGames } from "../../redux/actions";
 import validation from "./validate";
-import styles from "./Form.module.css";
 
 const Form = ({ allGenres, allPlatforms }) => {
   const allVideogames = useSelector((state) => state.allGames);
@@ -36,15 +37,78 @@ const Form = ({ allGenres, allPlatforms }) => {
     setErrors(validation({ ...gameData, [prop]: value }, allVideogames));
   };
 
-  const handleSubmit = (event) => {
-    if (gameData.name === "") event.preventDefault(); // Sirve para que la página no haga refresh por defecto.
-    dispatch(createGames(gameData));
+  const AddPlatform = (event) => {
+    const isChecked = event.target.checked;
+    const Value = event.target.value;
+    if (isChecked) {
+      setGameData({
+        ...gameData,
+        platforms: [...gameData.platforms, event.target.value],
+      });
+      setErrors(
+        validation(
+          {
+            ...gameData,
+            platforms: [...gameData.platforms, event.target.value],
+          },
+          allVideogames
+        )
+      );
+    } else {
+      setGameData({
+        ...gameData,
+        platforms: gameData.platforms.filter((plat) => plat !== Value),
+      });
+      setErrors(
+        validation(
+          {
+            ...gameData,
+            platforms: gameData.platforms.filter((plat) => plat !== Value),
+          },
+          allVideogames
+        )
+      );
+    }
+  };
+  const AddGenres = (event) => {
+    const isChecked = event.target.checked;
+    const Value = event.target.value;
+    if (isChecked) {
+      setGameData({
+        ...gameData,
+        genres: [...gameData.genres, event.target.value],
+      });
+      setErrors(
+        validation(
+          { ...gameData, genres: [...gameData.genres, event.target.value] },
+          allVideogames
+        )
+      );
+    } else {
+      setGameData({
+        ...gameData,
+        genres: gameData.genres.filter((gen) => gen !== Value),
+      });
+      setErrors(
+        validation(
+          {
+            ...gameData,
+            genres: gameData.genres.filter((gen) => gen !== Value),
+          },
+          allVideogames
+        )
+      );
+    }
   };
 
+  const handleSubmit = (event) => {
+    if (gameData.name === "") event.preventDefault(); //Sirve para que la página no haga refresh por default.
+    dispatch(createGames(gameData));
+  };
   const ratingInCero = (event) => {
     if (!gameData.rating) event.target.value = 0;
   };
-  console.log(allGenres);
+
   return (
     <div className={styles.globalCont}>
       <div className={styles.contForm}>
@@ -65,28 +129,32 @@ const Form = ({ allGenres, allPlatforms }) => {
             name="image"
             onChange={handleChange}
             value={gameData.image}
-          />
+          ></input>
 
           <label htmlFor="description">Description:</label>
           <textarea
             name="description"
             onChange={handleChange}
             value={gameData.description}
-          />
+          ></textarea>
 
           <label htmlFor="platforms">Platforms:</label>
-          <select
-            name="platforms"
-            multiple
-            onChange={handleChange}
-            value={gameData.platforms}
-          >
-            {allPlatforms.map((platform) => (
-              <option key={platform.id} value={platform.id}>
-                {platform.name}
-              </option>
-            ))}
-          </select>
+          <div className={styles.contPlatforms}>
+            {allPlatforms?.map((plat, index) => {
+              return (
+                <div key={index}>
+                  <input
+                    type="checkbox"
+                    onClick={(event) => AddPlatform(event)}
+                    value={plat}
+                  />
+                  <label key={plat} htmlFor={plat}>
+                    {plat}
+                  </label>
+                </div>
+              );
+            })}
+          </div>
 
           <label htmlFor="released">Released:</label>
           <input
@@ -94,7 +162,7 @@ const Form = ({ allGenres, allPlatforms }) => {
             name="released"
             onChange={handleChange}
             value={gameData.released}
-          />
+          ></input>
 
           <label htmlFor="rating">Rating</label>
           <input
@@ -104,36 +172,34 @@ const Form = ({ allGenres, allPlatforms }) => {
             onChange={handleChange}
             step="0.01"
             value={gameData.rating}
-          />
+          ></input>
 
-          <label htmlFor="genres">Choose your favorite genres:</label>
-          <select
-            name="genres"
-            multiple
-            onChange={handleChange}
-            value={gameData.genres}
-          >
-            {Array.isArray(allGenres) &&
-              allGenres.map((genre) => (
-                <option key={genre.id} value={genre.id}>
-                  {genre.name}
-                </option>
-              ))}
-          </select>
-
+          <label htmlFor="genres">Choose your favorites Genres:</label>
+          <div className={styles.contGenres}>
+            {allGenres?.map((genre, index) => {
+              return (
+                <div key={index}>
+                  <input
+                    type="checkbox"
+                    onClick={(event) => AddGenres(event)}
+                    value={genre}
+                  />
+                  <label htmlFor={genre}>{genre}</label>
+                </div>
+              );
+            })}
+          </div>
           {errors.flag === true ? (
-            <button disabled className={styles.disabledButton}>
-              Create Game
-            </button>
+            <button disabled>Create Game</button>
           ) : (
-            <button className={styles.button}>Create Game</button>
+            <button>Create Game</button>
           )}
         </form>
       </div>
 
       <div className={styles.infoValidation}>
         <div className={styles.titleInfo}>
-          <h2>📌 Validations:</h2>
+          <h2>📌 Validaciones:</h2>
           <p>
             - Deberán cumplirse las condiciones de validación para almacenar el
             nuevo videojuego en la base de datos, de lo contrario, los datos no
