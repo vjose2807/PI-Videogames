72e9ca3b6393dc1f317f2abcfef597a8b10f33a3
Jose Lizandro Vargas Ollisco <vjose2807@gmail.com>
2023-06-14 01:20:32 -0400

    Sin cambios

--- PI-Videogames-main/client/src/components/index.js
+++ PI-Videogames-main/client/src/components/index.js
@@ -1,9 +1,6 @@
-import Footer from "./Navbar/Navbar";
 import Navbar from "./Navbar/Navbar";
-
 import Card from "./Card/Card";
 import Paginator from "./Paginator/Paginator";
-import Filters from "./Filters/Filters"
+import Filters from "./Filters/Filters";
 
-
-export {Footer, Paginator, Navbar, Card, Filters};
+export { Paginator, Navbar, Card, Filters };
