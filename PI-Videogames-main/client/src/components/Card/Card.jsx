18805a72af62cd4934389739fe7c2339beff69a0
Jose Lizandro Vargas Ollisco <vjose2807@gmail.com>
2023-06-14 01:13:56 -0400

    Solucionado (card no mostraba generos)

--- PI-Videogames-main/client/src/components/Card/Card.jsx
+++ PI-Videogames-main/client/src/components/Card/Card.jsx
@@ -1,31 +1,26 @@
+import React from "react";
 import style from "./Card.module.css";
 import { Link } from "react-router-dom";
 
 const Card = ({ id, name, genres, image, rating, createinDb }) => {
   return (
     <div className={style.Card}>
-      {createinDb === false ? (
-        <>
-          <Link to={`/detail/${id}`}>
-            <h4>{name}</h4>
-          </Link>
-          <img className={style.imgCards} src={image} alt="img" />
-          <p>Genre: {genres?.toString()}</p>
-          <p>Rating: {rating}</p>
-        </>
-      ) : (
-        <>
-          <Link to={`/detail/${id}`}>
-            <h4>{name}</h4>
-          </Link>
-          <img className={style.imgCards} src={image} alt="img" />
-          <p>Genre: {genres?.map((genre) => genre.name).toString()}</p>
-          <p>Rating: {rating.toString().slice(0, 4)}</p>{" "}
-          {/* Convertir rating a cadena de texto */}
-        </>
-      )}
+      <Link to={`/detail/${id}`}>
+        <h4>{name}</h4>
+      </Link>
+      <img className={style.imgCards} src={image} alt="img" />
+      <p>Genre: {getGenres(genres, createinDb)}</p>
+      <p>Rating: {createinDb ? rating.toString().slice(0, 4) : rating}</p>
     </div>
   );
+};
+
+const getGenres = (genres, createinDb) => {
+  if (createinDb) {
+    return genres.map((genre) => genre.name).join(", ");
+  } else {
+    return genres.join(", ");
+  }
 };
 
 export default Card;
