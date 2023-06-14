65275fdc172dbbc93a8880019581b72b1ce1efb5
Jose Lizandro Vargas Ollisco <vjose2807@gmail.com>
2023-06-14 01:15:25 -0400

    al tocar Home vuelven a cargar los videojuegos

--- PI-Videogames-main/client/src/components/Navbar/Navbar.jsx
+++ PI-Videogames-main/client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
 import style from "./Navbar.module.css";
+import { useDispatch } from "react-redux";
+import { getAllGames } from "../../redux/actions";
 
 import Searchbar from "../Searchbar/Searchbar";
 
 const Navbar = ({ setCurrentPage }) => {
+  const dispatch = useDispatch();
+
+  const handleHomeClick = () => {
+    dispatch(getAllGames());
+    setCurrentPage(1);
+  };
+
   return (
     <div className={style.contBarra}>
       <div className={style.contOptions}>
         <Link to="/home">
-          <p>HOME</p>
+          <p onClick={handleHomeClick}>HOME</p>
         </Link>
 
         <Link to="/create">
