c956792abbdb927cc6d88038e2e4f20ed420f5aa
Jose Lizandro Vargas Ollisco <vjose2807@gmail.com>
2023-06-14 01:10:59 -0400

    Solucionado (no mostraba las platforms)

--- PI-Videogames-main/client/src/views/Detail/Detail.jsx
+++ PI-Videogames-main/client/src/views/Detail/Detail.jsx
@@ -3,14 +3,16 @@ import style from "./Detail.module.css";
 
 const Detail = () => {
   const game = useGameDetail();
+  const backgroundImageStyle = {
+    backgroundImage: `url(${game.image})`,
+  };
 
   return (
-    <div className={style.contGame}>
+    <div className={style.contGame} style={backgroundImageStyle}>
       {game.name ? (
         <>
           <div className={style.contAllInfo}>
             <div className={style.contTitleImg}>
-              <h2>Game Information</h2>
               <h3>{game.name}</h3>
               <img className={style.imgGame} src={game.image} alt="" />
             </div>
@@ -19,33 +21,23 @@ const Detail = () => {
               {game.createinDb === false ? (
                 <>
                   <p>ID: {game.id}</p>
-                  <p>
-                    Platforms:{" "}
-                    {game.platforms
-                      ?.map((platform) => platform.name)
-                      .join(", ")}
-                  </p>
+                  <p>Platforms: {game.platforms?.join(", ")}</p>
                   <p>Description:</p>
                   <div dangerouslySetInnerHTML={{ __html: game.description }} />
                   <p>Released: {game.released}</p>
                   <p>Rating: {String(game.rating).slice(0, 4)}</p>
-                  <p>Genres: {game.genres?.toString()}</p>
+                  <p>Genres: {game.genres?.join(", ")}</p>
                 </>
               ) : (
                 <>
                   <p>ID: {game.id}</p>
-                  <p>
-                    Platforms:{" "}
-                    {game.platforms
-                      ?.map((platform) => platform.name)
-                      .join(", ")}
-                  </p>
+                  <p>Platforms: {game.platforms?.join(", ")}</p>
                   <p>Description:</p>
                   <div dangerouslySetInnerHTML={{ __html: game.description }} />
                   <p>Released: {game.released}</p>
                   <p>Rating: {String(game.rating).slice(0, 4)}</p>
                   <p>
-                    Genres: {game.genres?.map((genre) => genre.name).toString()}
+                    Genres: {game.genres?.map((genre) => genre.name).join(", ")}
                   </p>
                 </>
               )}
